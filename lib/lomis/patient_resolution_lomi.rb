969eb22a9f60be7d6c788a6571678e5ec36416e6
Nicolas Bravo <nbravo@mit.edu>
2014-01-21 16:35:16 -0800

    propagate resolved_patient_ids

--- lib/lomis/patient_resolution_lomi.rb
+++ lib/lomis/patient_resolution_lomi.rb
@@ -13,9 +13,8 @@ module Chla
     def run
       @db = @mongo_client.db("picu_cerner_match")
       patients = @db["patients"]
-      patients.find(:source => "picudb").each do |patient|
-        patients.update({mrn: patient["mrn"]}, {"$set" => {"resolved_patient_id" => (0...8).map{(65 + rand(26)).chr}.join}}, {:multi => true})
-      end
+      assign_patient_ids(patients)
+      propagate_resolved_patient_ids(patients)
       puts patients.find.to_a
       puts "running #{self.class}"
     end
@@ -24,6 +23,25 @@ module Chla
       # TODO: Finish and clean up here.  Some examples:
       # move files, delete directories, close connections, log status, send notifications
       puts "finishing #{self.class}"
+    end
+
+    def assign_patient_ids(patients)
+      patients.find(:source => "picudb").each do |patient|
+        patients.update({mrn: patient["mrn"]}, {"$set" => {"resolved_patient_id" => (0...8).map{(65 + rand(26)).chr}.join}}, {:multi => true})
+      end
+    end
+
+    def propagate_resolved_patient_ids(patients)
+      encounters = @db["encounters"]
+      events = @db["events"]
+      patients.find().each do |patient|
+        patient_id = patient["_id"]
+        encounters.update({:patient_id => patient_id}, {"$set" => {"resolved_patient_id" => patient["resolved_patient_id"]}}, {:multi => true})
+        encounters.find({:patient_id => patient_id}).each do |encounter|
+          encounter_id = encounter["_id"]
+          events.update({:encounter_id => encounter_id}, {"$set" => {"resolved_patient_id" => patient["resolved_patient_id"]}}, {:multi => true})
+        end
+      end
     end
 
   end
