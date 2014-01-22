0fe5b48d9417a8ed32d6a69766c9dcd5068b1797
Nicolas Bravo <nbravo@mit.edu>
2014-01-22 15:56:04 -0800

    time matching works

--- lib/lomis/patient_resolution_lomi.rb
+++ lib/lomis/patient_resolution_lomi.rb
@@ -1,3 +1,4 @@
+require 'securerandom'
 require 'mongo'
 include Mongo
 
@@ -12,8 +13,8 @@ module Chla
     def run
       @db = @mongo_client.db("picu_cerner_match")
       patients = @db["patients"]
-      #assign_patient_ids(patients)
-      #propagate_resolved_patient_ids(patients)
+      assign_patient_ids(patients)
+      propagate_resolved_patient_ids(patients)
       match_dates(patients)
       puts "running #{self.class}"
     end
@@ -26,7 +27,7 @@ module Chla
 
     def assign_patient_ids(patients)
       patients.find(:source => "picudb").each do |patient|
-        patients.update({mrn: patient["mrn"]}, {"$set" => {"resolved_patient_id" => (0...8).map{(65 + rand(26)).chr}.join}}, {:multi => true})
+        patients.update({mrn: patient["mrn"]}, {"$set" => {"resolved_patient_id" => SecureRandom.hex}}, {:multi => true})
       end
     end
 
@@ -36,30 +37,49 @@ module Chla
       patients.find().each do |patient|
         patient_id = patient["_id"]
         encounters.update({:patient_id => patient_id}, {"$set" => {"resolved_patient_id" => patient["resolved_patient_id"]}}, {:multi => true})
-        encounters.find({:patient_id => patient_id}).each do |encounter|
-          encounter_id = encounter["_id"]
-          events.update({:encounter_id => encounter_id}, {"$set" => {"resolved_patient_id" => patient["resolved_patient_id"]}}, {:multi => true})
-        end
+        events.update({:patient_id => patient_id}, {"$set" => {"resolved_patient_id" => patient["resolved_patient_id"]}}, {:multi => true})
       end
     end
 
     def match_dates(patients)
-      puts 'match dates'
       events = @db["events"]
       encounters = @db["encounters"]
-      events.find().each do |event|
-        resolved_patient_id = event["resolved_patient_id"]
-        patients.find({:resolved_patient_id => resolved_patient_id, :source => "picudb"}).each do |patient|
-          patient_encounters = encounters.find({:patient_id => patient["_id"]})
-          patient_encounters.each do |patient_encounter|
-            if within_time_interval(event, encounter)
-              events.update({:_id => event["_id"]}, {"$set" => {:resolved_encounter_id => encounter["_id"]}})
+      patients.find({:source => "cerner_patients"}).each do |cerner_patient|
+        if cerner_patient["resolved_patient_id"].nil?
+          next
+        end
+        picudb_patient_id = patients.find({:resolved_patient_id => cerner_patient["resolved_patient_id"], :source => "picudb"}).to_a.first["_id"]
+        cerner_events = events.find({:patient_id => cerner_patient["_id"]})
+        cerner_events.each do |cerner_event|
+          encounters.find({:patient_id => picudb_patient_id}).each do |encounter|
+            if within_time_interval(cerner_event, encounter)
+              events.update({:_id => cerner_event["_id"]}, {"$set" => {:resolved_encounter_id => encounter["_id"]}})
               break
             end
           end
         end
       end
-      puts 'done with match dates'
+    end
+
+    # This method checks whether a given event occurs during a given encounter.
+    # It requires that the encounter contain both an admission and a discharge
+    # date. If the event does not have an end_time (and by assumption, does not
+    # have a start_time either), then this method returns false. Otherwise,
+    # returns true if the event's start_time and end_time both fall in between
+    # the encounter's admission and discharge dates.
+    def within_time_interval(event, encounter)
+      within = false
+      encounter_start = encounter["admission"]
+      encounter_end = encounter["discharge"]
+      unless event["end_time"].nil? or encounter_start.nil? or encounter_end.nil?
+        if event["start_time"].nil?
+          within = event["end_time"] >= encounter_start and event["end_time"] <= encounter_end
+        else
+          within = event["end_time"] >= encounter_start and event["end_time"] <= encounter_end and event["start_time"] >= encounter_start and event["start_time"] <= encounter_end
+
+        end
+      end
+      return within
     end
 
   end
