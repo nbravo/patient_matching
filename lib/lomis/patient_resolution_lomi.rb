7f83ec4b3ace6287db94d117ab7e13e0dc1886f5
Nicolas Bravo <nbravo@mit.edu>
2014-01-22 16:44:40 -0800

    reorganized initializations

--- lib/lomis/patient_resolution_lomi.rb
+++ lib/lomis/patient_resolution_lomi.rb
@@ -7,50 +7,46 @@ module Chla
 
     def initialize(options = {})
       @mongo_client = MongoClient.new("sherylj", 27017)
+      @db = @mongo_client.db("picu_cerner_match")
+      @patients = @db["patients"]
+      @encounters = @db["encounters"]
+      @events = @db["events"]
       puts "initializing #{self.class}"
     end
 
     def run
-      @db = @mongo_client.db("picu_cerner_match")
-      patients = @db["patients"]
-      assign_patient_ids(patients)
-      propagate_resolved_patient_ids(patients)
-      match_dates(patients)
+      assign_patient_ids
+      propagate_resolved_patient_ids
+      resolve_encounter_ids
       puts "running #{self.class}"
     end
 
     def finish
-      # TODO: Finish and clean up here.  Some examples:
-      # move files, delete directories, close connections, log status, send notifications
       puts "finishing #{self.class}"
     end
 
-    def assign_patient_ids(patients)
-      patients.find({:source => "picudb"}).each do |patient|
-        patients.update({mrn: patient["mrn"]}, {"$set" => {"resolved_patient_id" => SecureRandom.hex}}, {:multi => true})
+    def assign_patient_ids
+      @patients.find({:source => "picudb"}).each do |patient|
+        @patients.update({mrn: patient["mrn"]}, {"$set" => {"resolved_patient_id" => SecureRandom.hex}}, {:multi => true})
       end
     end
 
-    def propagate_resolved_patient_ids(patients)
-      encounters = @db["encounters"]
-      events = @db["events"]
-      patients.find().each do |patient|
+    def propagate_resolved_patient_ids
+      @patients.find().each do |patient|
         patient_id = patient["_id"]
-        encounters.update({:patient_id => patient_id}, {"$set" => {"resolved_patient_id" => patient["resolved_patient_id"]}}, {:multi => true})
-        events.update({:patient_id => patient_id}, {"$set" => {"resolved_patient_id" => patient["resolved_patient_id"]}}, {:multi => true})
+        @encounters.update({:patient_id => patient_id}, {"$set" => {"resolved_patient_id" => patient["resolved_patient_id"]}}, {:multi => true})
+        @events.update({:patient_id => patient_id}, {"$set" => {"resolved_patient_id" => patient["resolved_patient_id"]}}, {:multi => true})
       end
     end
 
-    def match_dates(patients)
-      events = @db["events"]
-      encounters = @db["encounters"]
-      patients.find({:source => "cerner_patients", :resolved_patient_id => {"$exists" => true}}).each do |cerner_patient|
-        picudb_patient_id = patients.find_one({:resolved_patient_id => cerner_patient["resolved_patient_id"], :source => "picudb"})["_id"]
-        cerner_events = events.find({:patient_id => cerner_patient["_id"]})
+    def resolve_encounter_ids
+      @patients.find({:source => "cerner_patients", :resolved_patient_id => {"$exists" => true}}).each do |cerner_patient|
+        picudb_patient_id = @patients.find_one({:resolved_patient_id => cerner_patient["resolved_patient_id"], :source => "picudb"})["_id"]
+        cerner_events = @events.find({:patient_id => cerner_patient["_id"]})
         cerner_events.each do |cerner_event|
-          encounters.find({:patient_id => picudb_patient_id}).each do |encounter|
+          @encounters.find({:patient_id => picudb_patient_id}).each do |encounter|
             if within_time_interval(cerner_event, encounter)
-              events.update({:_id => cerner_event["_id"]}, {"$set" => {:resolved_encounter_id => encounter["_id"]}})
+              @events.update({:_id => cerner_event["_id"]}, {"$set" => {:resolved_encounter_id => encounter["_id"]}})
               break
             end
           end
