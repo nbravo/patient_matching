cb6e2498df3ed59e40493ee4258667ff8a342b5a
Nicolas Bravo <nbravo@mit.edu>
2014-01-24 14:32:58 -0800

    reorganized some stuff

--- lib/lomis/patient_resolution_lomi.rb
+++ lib/lomis/patient_resolution_lomi.rb
@@ -5,11 +5,9 @@ include Mongo
 
 module Chla
   class PatientResolutionLomi
-    attr_reader :patients, :encounters, :events
-    attr_writer :patients, :encounters, :events
+    attr_accessor :patients, :encounters, :events
 
     def initialize(options = {:env => "development"})
-      puts "initializing #{self.class}"
       read_config(options)
       @mongo_client = MongoClient.new(@host, @port)
       @db = @mongo_client.db(@database)
@@ -19,14 +17,12 @@ module Chla
     end
 
     def run
-      puts "running #{self.class}"
       resolve_patient_ids
       propagate_resolved_patient_ids
       resolve_encounter_ids
     end
 
     def finish
-      puts "finishing #{self.class}"
     end
 
     def read_config(options)
@@ -39,22 +35,28 @@ module Chla
 
     def resolve_patient_ids
       @patients.find({:source => "picudb"}).each do |patient|
-        @patients.update({mrn: patient["mrn"]}, {"$set" => {"resolved_patient_id" => SecureRandom.hex}}, {:multi => true})
+        @patients.update(patient_resolution_selector(patient),
+            {:$set => {:resolved_patient_id => SecureRandom.hex}},
+            {:multi => true})
       end
+    end
+
+    def patient_resolution_selector(patient)
+      {mrn: patient["mrn"]}
     end
 
     def propagate_resolved_patient_ids
       @patients.find().each do |patient|
         patient_id = patient["_id"]
-        @encounters.update({:patient_id => patient_id}, {"$set" => {"resolved_patient_id" => patient["resolved_patient_id"]}}, {:multi => true})
-        @events.update({:patient_id => patient_id}, {"$set" => {"resolved_patient_id" => patient["resolved_patient_id"]}}, {:multi => true})
+        @encounters.update({:patient_id => patient_id}, {:$set => {:resolved_patient_id => patient["resolved_patient_id"]}}, {:multi => true})
+        @events.update({:patient_id => patient_id}, {:$set => {:resolved_patient_id => patient["resolved_patient_id"]}}, {:multi => true})
       end
     end
 
     def resolve_encounter_ids
       @encounters.find({:resolved_patient_id => {:$exists => true}, :source => "picudb"}).each do |picudb_encounter|
-        cerner_patient = @patients.find_one({:resolved_patient_id => picudb_encounter[:resolved_patient_id], :source => "cerner_patients"})
-        @events.update({:patient_id => cerner_patient["_id"], :end_time => {"$gte" => picudb_encounter["admission"], "$lt" => picudb_encounter["discharge"]}, :start_time => {"$gte" => picudb_encounter["admission"], "$lt" => picudb_encounter["discharge"]}}, {:$set => {:resolved_encounter_id => picudb_encounter["_id"]}}, {:multi => true})
+        cerner_patient = @patients.find_one({:resolved_patient_id => picudb_encounter["resolved_patient_id"], :source => "cerner_patients"})
+        @events.update({:patient_id => cerner_patient["_id"], :end_time => {:$gte => picudb_encounter["admission"], :$lt => picudb_encounter["discharge"]}, :start_time => {:$gte => picudb_encounter["admission"], :$lt => picudb_encounter["discharge"]}}, {:$set => {:resolved_encounter_id => picudb_encounter["_id"]}}, {:multi => true})
       end
     end
   end
--- test/lomis/patient_resolution_lomi_test.rb
+++ test/lomis/patient_resolution_lomi_test.rb
@@ -1,4 +1,5 @@
 require 'test/unit'
+require 'time'
 require 'chla'
 
 module Chla
@@ -19,14 +20,11 @@ module Chla
       @alice_picudb_id = @lomi.patients.insert({:mrn => @alice_mrn, :source => "picudb", :first_name => "Alice"})
       @alice_cerner_id = @lomi.patients.insert({:mrn => @alice_mrn, :source => "cerner_patients", :first_name => "Alice"})
 
-      #bob_picudb_id = @lomi.patients.insert({:mrn => 456, :source => "picudb", :first_name => "Bob"})
-      #bob_cerner_id = @lomi.patients.insert({:mrn => 456, :source => "cerner_patients", :first_name => "Bob"})
-
-      @alice_picudb_encounter_id = @lomi.encounters.insert({:patient_id => @alice_picudb_id, :source => "picudb"})
+      @alice_picudb_encounter_id = @lomi.encounters.insert({:patient_id => @alice_picudb_id, :source => "picudb", :admission => Time.new(2000, 12, 25), :discharge => Time.new(2000, 12, 28)})
       @alice_cerner_encounter_id = @lomi.encounters.insert({:patient_id => @alice_cerner_id, :source => "cerner_paients"})
 
       @alice_picudb_event_id = @lomi.events.insert({:patient_id => @alice_picudb_id, :encounter_id => @alice_picudb_encounter_id})
-      @alice_cerner_event_id = @lomi.events.insert({:patient_id => @alice_cerner_id, :encounter_id => @alice_cerner_encounter_id})
+      @alice_cerner_event_id = @lomi.events.insert({:patient_id => @alice_cerner_id, :encounter_id => @alice_cerner_encounter_id, :start_time => Time.new(2000, 12, 26), :end_time => Time.new(2000, 12, 26) })
     end
 
     def clear_test_data
@@ -50,10 +48,12 @@ module Chla
 
     def test_propagate_resolved_patient_ids
       resolved_patient_id = SecureRandom.hex
-      @lomi.patients.update({mrn: @alice_mrn}, {"$set" => {"resolved_patient_id" => resolved_patient_id}}, {:multi => true})
+      @lomi.patients.update({mrn: @alice_mrn}, {:$set => {:resolved_patient_id => resolved_patient_id}}, {:multi => true})
       assert_equal @lomi.encounters.find({:resolved_patient_id => {:$exists => true}}).count(), 0
       assert_equal @lomi.events.find({:resolved_patient_id => {:$exists => true}}).count(), 0
       @lomi.propagate_resolved_patient_ids
+      puts @alice_picudb_encounter_id
+      puts @lomi.encounters.find().to_a
       assert_equal @lomi.encounters.find_one({:_id => @alice_picudb_encounter_id})["resolved_patient_id"], resolved_patient_id
       assert_equal @lomi.encounters.find_one({:_id => @alice_cerner_encounter_id})["resolved_patient_id"], resolved_patient_id
       assert_equal @lomi.events.find_one({:_id => @alice_picudb_event_id})["resolved_patient_id"], resolved_patient_id
@@ -61,6 +61,16 @@ module Chla
     end
 
     def test_resolve_encounter_ids
+      resolved_patient_id = SecureRandom.hex
+      @lomi.patients.update({mrn: @alice_mrn}, {:$set => {:resolved_patient_id => resolved_patient_id}}, {:multi => true})
+      @lomi.encounters.update({:_id => @alice_picudb_encounter_id}, {:$set => {:resolved_patient_id => resolved_patient_id}})
+      @lomi.encounters.update({:_id => @alice_cerner_encounter_id}, {:$set => {:resolved_patient_id => resolved_patient_id}})
+      @lomi.events.update({:_id => @alice_picudb_event_id}, {:$set => {:resolved_patient_id => resolved_patient_id}})
+      @lomi.events.update({:_id => @alice_cerner_event_id}, {:$set => {:resolved_patient_id => resolved_patient_id}})
+      assert_equal @lomi.events.find({:resolved_encounter_id => {:$exists => true}}).count(), 0
+      @lomi.resolve_encounter_ids
+      assert_equal @lomi.events.find_one({:_id => @alice_cerner_event_id})["resolved_encounter_id"], @lomi.encounters.find_one({:_id => @alice_cerner_encounter_id})["_id"]
+      assert_equal @lomi.events.find({:_id => @alice_picudb_event_id, :resolved_encounter_id => {:$exists => true}}).count(), 0
     end
   end
 end
